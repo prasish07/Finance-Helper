b0e3a1d5060c784ef4ed2e2c21ac13e44658de44
prasish07 <prasishshrestha0099@gmail.com>
2024-08-18 17:23:26 +0545

    Setting routes

---
+++ app/(dashboard)/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
+import React from "react";
+
+const Main = () => {
+  return <UserButton />;
+};
+
+export default Main;
